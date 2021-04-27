615327b905b85bcdd1174bf287bc0f16efa361cb
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-04-27 21:57:17 +0200

    add form for users to reserve a trip, add Jquery to forbid users to change several fields

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Trip;
-use App\Repository\SpacecraftRepository;
+use App\Form\TripType;
 use App\Repository\TripRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SpacecraftRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TripController extends AbstractController
 {
@@ -19,11 +22,34 @@ class TripController extends AbstractController
     public function index(TripRepository $repo): Response
     {
         $trips = $repo->findAll();
-
         return $this->render('trip/index.html.twig', [
             'trips' => $trips
         ]);
     }
+
+    /**
+     * Create a reserved trip
+     * @Route("/trips/create",name="app_trip_create")
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $trip = new Trip;
+        $trip->setReserved(true);
+        $form = $this->createForm(TripType::class, $trip);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trip = $form->getData();
+            $trip->setReserved(true);
+            $em->persist($trip);
+            $em->flush();
+            return $this->redirectToRoute('app_admin_home');
+        }
+        return $this->render('trip/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
 
     /**
      * Show one trip by his name
