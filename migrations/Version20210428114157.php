c21b107b8fc4b903e6212e018cda8dda1914cd7d
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-04-28 15:30:08 +0200

    change spacecrafe_id and user_id to not null for feedback field

--- migrations/Version20210428114157.php
+++ migrations/Version20210428114157.php
@@ -20,7 +20,7 @@ final class Version20210428114157 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE feedback (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, spacecraft_id INT DEFAULT NULL, content LONGTEXT NOT NULL, INDEX IDX_D2294458A76ED395 (user_id), INDEX IDX_D22944581C6AF6FD (spacecraft_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE feedback (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, spacecraft_id INT NOT NULL, content LONGTEXT NOT NULL, INDEX IDX_D2294458A76ED395 (user_id), INDEX IDX_D22944581C6AF6FD (spacecraft_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D2294458A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D22944581C6AF6FD FOREIGN KEY (spacecraft_id) REFERENCES spacecraft (id)');
