89bc33afecd3da85d293f97ed33448b79efbd341
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-04-29 17:45:39 +0200

    Change the label for all forms

--- src/Form/SpacecraftType.php
+++ src/Form/SpacecraftType.php
@@ -3,23 +3,44 @@
 namespace App\Form;
 
 use App\Entity\Spacecraft;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SpacecraftType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('possibleDestination')
-            ->add('brand')
-            ->add('numberOfSeat')
-            ->add('nationality')
-            ->add('description')
-            ->add('speed');
+            ->add('name', TextType::class, [
+                'label' => 'Nom du vaisseau',
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Prix du vaisseau'
+            ])
+            ->add('possibleDestination', TextType::class, [
+                'label' => 'Destinations possibles avec ce vaisseau'
+            ])
+            ->add('brand', TextType::class, [
+                'label' => 'Marque du vaisseau'
+            ])
+            ->add('numberOfSeat', IntegerType::class, [
+                'label' => 'Nombres de sièges disponible'
+            ])
+            ->add('nationality', TextType::class, [
+                'label' => 'Lieu de création du vaisseau'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du vaisseau'
+            ])
+            ->add('speed', NumberType::class, [
+                'label' => 'Vitesse du vaisseau (en km/h)'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/TripType.php
+++ src/Form/TripType.php
@@ -7,23 +7,43 @@ use App\Entity\Spacecraft;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TripType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('destination')
-            ->add('departureAt')
-            ->add('arrivalAt')
-            ->add('availableSeatNumber')
-            ->add('reserved')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du voyage'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du voyage'
+            ])
+            ->add('destination', TextType::class, [
+                'label' => 'Destination du voyage'
+            ])
+            ->add('departureAt', DateTimeType::class, [
+                'label' => 'Date de départ du voyage'
+            ])
+            ->add('arrivalAt', DateTimeType::class, [
+                'label' => 'Date d\'arrivée du voyage'
+            ])
+            ->add('availableSeatNumber', IntegerType::class, [
+                'label' => 'Nombre de places disponibles'
+            ])
+            ->add('reserved', CheckboxType::class, [
+                'label' => 'Voyage reservé'
+            ])
             ->add('spacecraft', EntityType::class, [
                 'class' => Spacecraft::class,
-                'choice_label' => 'name'
+                'choice_label' => 'name',
+                'label' => 'Vaisseau utilisé'
             ]);
     }
 
