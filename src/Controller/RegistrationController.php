ff9336bcb874d903dc51433879af80cbc4d8a978
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-07-27 22:07:30 +0200

    Add buttons for registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,11 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, ConnectionFormAuthenticator $authenticator): Response
     {
+        //Redirect to home page if user is already connected
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
